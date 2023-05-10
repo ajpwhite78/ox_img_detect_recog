fc1d691e096e0dbcd89f4bf91b800ef97e89cf39
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-10 14:40:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
-webrtc_streamer(key="sample", rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+webrtc_streamer(key="sample", rtc_configuration={"iceServers": [{"urls": ["stun:stun-eu.3cx.com:3478"]}]})
