daff1777d7208cb8467ab60456f1c0a4e56c6825
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-10 09:46:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-st.camera_input(label="", label_visibility="collapsed")
+def process_frame(frame):
+    # Do some processing on the frame here
+    processed_frame = frame  # Replace with your actual processing code
+    return processed_frame
+
+  frame_window = st.empty()
+
+while True:
+    # Read a frame from the camera as a PIL Image
+    frame = frame_window.camera_input(label="", label_visibility="collapsed")
+
+    # Process the frame
+    processed_frame = process_frame(frame)
+
+    # Show the processed frame in the Streamlit app
+    frame_window.image(processed_frame)
+
+    # Wait for a short time before processing the next frame
+    time.sleep(0.1)
+
