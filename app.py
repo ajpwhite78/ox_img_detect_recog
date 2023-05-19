324a7e685040d11b7617f77fea1f087a672c8080
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-18 19:52:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ RTC_CONFIGURATION = RTCConfiguration(
 webrtc_ctx = webrtc_streamer(
     key="WYH",
     rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
+    media_stream_constraints={"video": True, "audio": True},
 )
